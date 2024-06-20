40ad096cd4bcb0d8b7a1a576cf0b83bce5c27d3e
Realion033 <93594604+Realion033@users.noreply.github.com>
2024-06-21 00:01:57 +0900

    1201

--- ChainSawProject/Assets/Realion033/01.Scripts/Player/Player.cs
+++ ChainSawProject/Assets/Realion033/01.Scripts/Player/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -6,14 +7,30 @@ public class Player : MonoBehaviour
 
     private PlayerInput playerInput;
     private PlayerMovement playerMovement;
+    private PlayerDash playerDash;
 
     void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
         playerMovement = GetComponent<PlayerMovement>();
+        playerDash = GetComponent<PlayerDash>();
     }
 
     void Update()
+    {
+        Move();
+        Dash();
+    }
+
+    private void Dash()
+    {
+        if(playerInput.isDash == true)
+        {
+            playerDash.StartDash(playerInput.mousePos);
+        }
+    }
+
+    private void Move()
     {
         Vector2 moveVelocity = playerInput.GetMoveVelocity();
         playerMovement.Move(moveVelocity * speed);
--- ChainSawProject/Assets/Realion033/01.Scripts/Player/PlayerDash.cs
+++ ChainSawProject/Assets/Realion033/01.Scripts/Player/PlayerDash.cs
@@ -1,8 +1,61 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class PlayerDash : PlayerInput
 {
-    
+    [SerializeField] private float dashDistance = 5f; 
+    [SerializeField] private float dashSpeed = 20f;   
+
+    private Rigidbody2D rb;
+    private Vector2 dashTarget;
+    private bool isDashing = false;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (isDashing)
+        {
+            CheckDashTargetReached();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isDashing)
+        {   
+            Dash();
+        }
+    }
+
+
+    private void Dash()
+    {
+        Vector2 direction = (dashTarget - rb.position).normalized;
+        Debug.Log("adsf");
+        rb.velocity = direction * dashSpeed;
+    }
+
+    private void CheckDashTargetReached()
+    {
+
+        if (Vector2.Distance(rb.position, dashTarget) <= 0.5f)
+        {
+            isDashing = false;
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    public void StartDash(Vector3 mousePos)
+    {
+        mousePos.z = 0;  // Z 좌표는 무시
+
+        Vector2 direction = (mousePos - transform.position).normalized;
+
+        dashTarget = (Vector2)transform.position + direction * dashDistance;
+        isDashing = true;  // 대쉬 시작
+    }
 }
