389ee218b20b2db4d3ea710ddcb043a3bb891b1a
Realion033 <93594604+Realion033@users.noreply.github.com>
2024-08-29 18:55:02 +0900

    dd

---
+++ ChainSawProject/Assets/Realion033/01.Scripts/TestEnemy.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestEnemy : LivingEntity
+{
+    private void Awake()
+    {
+        health = 300;
+    }
+}
