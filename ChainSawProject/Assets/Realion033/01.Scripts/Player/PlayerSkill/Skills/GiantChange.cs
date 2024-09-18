561d5b5d112452e931ff862f78f5346acafb77cd
Realion033 <93594604+Realion033@users.noreply.github.com>
2024-09-19 02:52:14 +0900

    0919 am3
    거대화 만듬, 쉴드 개념 추가 필요함

--- ChainSawProject/Assets/Realion033/01.Scripts/Player/PlayerSkill/Skills/GiantChange.cs
+++ ChainSawProject/Assets/Realion033/01.Scripts/Player/PlayerSkill/Skills/GiantChange.cs
@@ -1,25 +1,53 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.Intrinsics;
 using UnityEngine;
-
 public class GiantChange : Skill
 {
-    public Transform player;
+    public float increadeValue;
+    public float changeTime;
 
-    private void Update()
+    //------not setting-------
+    public Transform player;
+    public PlayerAttack _playerAttack;
+    private float defaultRadius;
+
+    public override void Init()
+    {
+        base.Init();
+        _playerInput.isSkillUse += HandleSkillUse;
+
+        defaultRadius = _playerAttack.damageCasterRadius;
+    }
+
+    public override void HandleSkillUse()
     {
         if (_skillEnum == Skills.GiantChange)
         {
             if (PlayerCooldownManager.Instance.UseUlt())
             {
-                UltAttack();
+                StartCoroutine(startSkill());
             }
         }
     }
 
-    private void UltAttack()
+    private IEnumerator startSkill()
     {
-    
+        Giant();
+        yield return new WaitForSeconds(changeTime);
+        backGiant();
+    }
+
+
+    private void Giant()
+    {
+        player.transform.localScale = new Vector3(increadeValue, increadeValue, increadeValue);
+        _playerAttack.damageCasterRadius = increadeValue - (increadeValue / 2);
+    }
+    private void backGiant()
+    {
+        player.transform.localScale = new Vector3(1, 1, 1);
+        _playerAttack.damageCasterRadius = defaultRadius;
     }
 }
