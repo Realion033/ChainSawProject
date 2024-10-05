38d14ae25bceb590d4a0c0c0e38d6918b9965ddc
20103kimminsung <jjang15467@gmail.com>
2024-10-05 23:11:54 +0900

    EnemyNoDown

---
+++ ChainSawProject/Assets/Kimminsung/01.Script/GroundCheck.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    public Collision2D GroundChecker;
+    public LayerMask GroundLayer;
+    private Rigidbody2D rb;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.layer == GroundLayer)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+        }
+    }
+}
